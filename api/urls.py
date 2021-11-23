bac75f7d74cb8fa6863688a46b0acb24b4282c8e
Mikal Morello <mikal_morello@harvard.edu>
2021-11-22 22:34:29 -0500

    Create basic stock API endpoint with 5 day closing prices

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("stockpiles/<str:stockpile_id>", views.stockpile, name="stockpile"),
     path("users", views.users, name="users"),
     path("users/<str:user_id>", views.user, name="user"),
-    path("stocks/<str:stock_key>", views.stock, name="stock")
+    path("stocks/<str:symbol_key>", views.stock, name="stock")
 ]
--- api/views.py
+++ api/views.py
@@ -48,20 +48,54 @@ def user(request, user_id):
         return JsonResponse(user.serialize())
 
 
-def stock(request, stock_key):
+def stock(request, symbol_key):
     # Get Stock
     # AlphaVantage API Key
     API_KEY = config('ALPHAVANTAGE_API_KEY')
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock_key}&apikey={API_KEY}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol_key}&apikey={API_KEY}'
     r = requests.get(url)
-    stock = r.json()
+    # stock = r.json()
+    data = r.json()
+
+    stock = {
+        "symbol": data["Meta Data"]["2. Symbol"],
+        "last_refreshed": data["Meta Data"]["3. Last Refreshed"],
+        "currentvalue": list(data["Time Series (Daily)"].values())[0]["5. adjusted close"],
+        "currentkey": list(data["Time Series (Daily)"].keys())[0],
+        "daily":
+            [
+                {
+                    "date": list(data["Time Series (Daily)"].keys())[0],
+                    "price": list(data["Time Series (Daily)"].values())[0]["5. adjusted close"]
+                },
+                {
+                    "date": list(data["Time Series (Daily)"].keys())[1],
+                    "price": list(data["Time Series (Daily)"].values())[1]["5. adjusted close"]
+                },
+                {
+                    "date": list(data["Time Series (Daily)"].keys())[2],
+                    "price": list(data["Time Series (Daily)"].values())[2]["5. adjusted close"]
+                },
+                {
+                    "date": list(data["Time Series (Daily)"].keys())[3],
+                    "price": list(data["Time Series (Daily)"].values())[3]["5. adjusted close"]
+                },
+                {
+                    "date": list(data["Time Series (Daily)"].keys())[4],
+                    "price": list(data["Time Series (Daily)"].values())[4]["5. adjusted close"]
+                },
+        ]
+
+    }
+
+    print(list(data["Time Series (Daily)"].keys())[0])
 
     # For a GET request
     if request.method == "GET":
-        return JsonResponse(stock)
+        return JsonResponse(stock, safe=False)
 
     return render(request, "api/test.html", {
         "title": "stock name",
-        "stock_key": stock_key,
+        "symbol_key": symbol_key,
         "api": API_KEY
     })
