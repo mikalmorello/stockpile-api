c453ed9f1e66964fe0b5f283e9007db6f1e7da64
Mikal Morello <mikal_morello@harvard.edu>
2021-11-27 16:59:15 -0500

    Add create stock, refresh stock, refresh stockpile helper functions

--- api/util.py
+++ api/util.py
@@ -1,13 +1,19 @@
 # Dependencies
 import requests
+import datetime
+import os
 from decouple import config
+from django.conf import settings
+
+
+# Impprt model data
+from .models import User, Stockpile, Symbol, Stock
+
 
 # Get stock data
-
-
-def get_stockdata(symbol_key):
+def get_stockdata(stock_symbol):
     API_KEY = config('ALPHAVANTAGE_API_KEY')
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol_key}&apikey={API_KEY}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock_symbol}&apikey={API_KEY}'
     r = requests.get(url)
     data = r.json()
 
@@ -34,4 +40,86 @@ def get_stockdata(symbol_key):
         },
     ]
 
+    # Return the stock data
     return stockdata
+
+
+# Update a stock
+def refresh_stock(stock_symbol):
+    print(f"--- {stock_symbol} Checked ---")
+    # Get Stock
+    stock = Stock.objects.get(symbol=stock_symbol.upper())
+    # Get todays date
+    todays_date = datetime.date.today()
+    # Get the stocks last update date
+    stock_date = stock.last_refreshed.date()
+
+    # If the stock hasn't been refreshed today
+    if not stock_date == todays_date:
+        print(f"--- {stock_symbol} Refreshed ---")
+        # Refresh stock data
+        stockdata = get_stockdata(stock.symbol)
+
+        # Update the stock data
+        stock.daily = stockdata
+        # Update the last refreshed data
+        stock.refreshed = datetime.datetime.now()
+        # Save the stock
+        stock.save()
+
+    # Return the stock
+    return stock
+
+
+# Refresh stockpile
+def refresh_stockpile(stockpile):
+    for stock in stockpile.stocks:
+        refresh_stock(stock.symbol)
+
+
+# Create a stock
+def create_stock(stock_symbol):
+    print(f"--- {stock_symbol} Created ---")
+    # Get the stock data
+    stockdata = get_stockdata(stock_symbol)
+
+    # Create new stock
+    stock = Stock(symbol=stock_symbol.upper(),
+                  daily=stockdata, change_day=0, change_week=0)
+    stock.save()
+
+    # Return the stock
+    return stock
+
+
+def update_symbols():
+    # Get Symbols
+    symbols = Symbol.objects.all()
+
+    # Get Nasdaq symbols files
+    fileObject = open(os.path.join(settings.BASE_DIR, 'nasdaqlisted.txt'), "r")
+    # Split file by line break
+    listings = fileObject.readlines()
+    # Remove the first header row
+    listings = listings[1:]
+    # Remove the date added at the end
+    listings = listings[:-1]
+
+    # Loop through symbols
+    for listing in listings:
+        # Remove any empty spaces
+        listing = listing.strip()
+        # Split symbol data on divider
+        listing = listing.split("|")
+        # Create new listing symbol
+        listing_symbol = listing[0]
+        listing_name = listing[1]
+
+        # print(new_symbol)
+        if symbols.filter(symbol=listing_symbol).exists():
+            # If symbol already exists, don't do anything
+            pass
+        else:
+            # Otherwise add symbol
+            new_symbol = Symbol(symbol=listing_symbol, name=listing_name)
+            new_symbol.save()
--- api/views.py
+++ api/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from decouple import config
-import requests
-import datetime
-import os
-from django.conf import settings
 from . import util
 
 from .models import User, Stockpile, Symbol, Stock
@@ -29,15 +24,9 @@ def stockpile(request, stockpile_id):
     # Get stockpile
     stockpile = Stockpile.objects.get(id=stockpile_id)
     stocks = stockpile.stocks.all()
-    # print(stocks)
-    symbols = []
-    # # Add stock data to each symbol
-    # for symbol in stocks:
-    #     # Get Stock
-    #     stock = util.get_stock(symbol)
-    #     # print(stock)
 
-    # print(stockpile)
+    for stock in stocks:
+        util.refresh_stock(stock.symbol)
 
     # For a GET request
     if request.method == "GET":
@@ -81,39 +70,8 @@ def symbol(request, stock_symbol):
 
 
 def update_symbols(request):
-    # Get Stocks
-    symbols = Symbol.objects.all()
-
-    # Get Nasdaq symbols files
-    fileObject = open(os.path.join(settings.BASE_DIR, 'nasdaqlisted.txt'), "r")
-    # Split file by line break
-    listings = fileObject.readlines()
-    # Remove the first header row
-    listings = listings[1:]
-    # Remove the date added at the end
-    listings = listings[:-1]
-
-    # Loop through symbols
-    for listing in listings:
-        # Remove any empty spaces
-        listing = listing.strip()
-        # Split symbol data on divider
-        listing = listing.split("|")
-        # Create new listing symbol
-        listing_symbol = listing[0]
-        listing_name = listing[1]
-
-        # print(new_symbol)
-        if symbols.filter(symbol=listing_symbol).exists():
-            # If symbol already exists, don't do anything
-            pass
-        else:
-            # Otherwise add symbol
-            new_symbol = Symbol(symbol=listing_symbol, name=listing_name)
-            new_symbol.save()
-    return render(request, "api/test.html", {
-        "title": "symbols",
-    })
+    # Update stock symbols
+    util.update_symbols()
 
 
 def stocks(request):
@@ -132,34 +90,14 @@ def stock(request, stock_symbol):
 
         # Check if stock exists
         if Stock.objects.filter(symbol=stock_symbol.upper()).exists():
-
-            # Get Stock
-            stock = Stock.objects.get(symbol=stock_symbol.upper())
-            date_of_today = datetime.date.today()
-            stock_date = stock.last_refreshed.date()
-
-            print(datetime.datetime.now())
-            # If the stock hasn't been refreshed today
-            if not date_of_today == stock_date:
-                # Refresh stock data
-                stockdata = util.get_stockdata(stock_symbol)
-
-                # Update the stock
-                stock.daily = stockdata
-                stock.refreshed = datetime.datetime.now()
-                stock.save()
+            # If the stock exists, update its data
+            stock = util.refresh_stock(stock_symbol)
 
             # Return json
             return JsonResponse(stock.serialize(), safe=False)
         else:
-            print('stock is new')
-            # Get Stockdata
-            stockdata = util.get_stockdata(stock_symbol)
-
-            # Create new stock
-            new_stock = Stock(symbol=stock_symbol.upper(),
-                              daily=stockdata, change_day=0, change_week=0)
-            new_stock.save()
+            # Create a new stock
+            new_stock = util.create_stock(stock_symbol)
 
             # Return json
             return JsonResponse(new_stock.serialize(), safe=False)
