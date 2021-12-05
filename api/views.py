b0db1ad78470705167df0d29c328532f81da3fa2
Mikal Morello <mikal_morello@harvard.edu>
2021-12-04 19:19:28 -0500

    Add basic views and serializers for models

--- api/views.py
+++ api/views.py
@@ -6,9 +6,15 @@ from django.db import IntegrityError
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from . import util
 
 from .models import User, Stockpile, Symbol, Stock
+
+# Rest Framework
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, StockpileSerializer, SymbolSerializer, StockSerializer
 
 
 def index(request):
@@ -92,40 +98,48 @@ def register(request):
         return render(request, "api/register.html")
 
 
-def users(request):
-    # Get users
-    users = User.objects.all()
-
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse([user.serialize() for user in users], safe=False)
-
-
-def user(request, user_id):
-    # Get User
-    user = User.objects.get(id=user_id)
-
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse(user.serialize())
+class UsersView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get users data
+        users = User.objects.all()
+        # Serialize users
+        serializer = UserSerializer(users, many=True)
+        # Return response
+        return Response(serializer.data)
 
 
-def stockpiles(request):
-    # Get stockpiles
-    stockpiles = Stockpile.objects.all()
+class UserView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get URL parameter
+        user_id = kwargs.get("user_id")
+        # Get userdata
+        user = User.objects.get(id=user_id)
+        # Serialize user
+        serializer = UserSerializer(user)
+        # Return response
+        return Response(serializer.data)
 
-    # For GET request
-    if request.method == 'GET':
-        return JsonResponse([stockpile.serialize() for stockpile in stockpiles], safe=False)
+
+class StockpilesView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get stockpiles data
+        stockpiles = Stockpile.objects.all()
+        # Serialize stockpiles
+        serializer = StockpileSerializer(stockpiles, many=True)
+        # Return response
+        return Response(serializer.data)
 
 
-def stockpile(request, stockpile_id):
-    # Refresh stockpile
-    stockpile = Stockpile.objects.get(id=stockpile_id)
-
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse(stockpile.serialize())
+class StockpileView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get URL parameter
+        stockpile_id = kwargs.get("stockpile_id")
+        # Get stockpile data
+        stockpile = Stockpile.objects.get(id=stockpile_id)
+        # Serialize stockpile
+        serializer = StockpileSerializer(stockpile)
+        # Return response
+        return Response(serializer.data)
 
 
 @csrf_exempt
@@ -143,22 +157,26 @@ def create_stockpile(request):
     })
 
 
-def symbols(request):
-    # Get Symbols
-    symbols = Symbol.objects.all()
+class SymbolsView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get symbols
+        symbols = Symbol.objects.all()
+        # Serialize symbols
+        serializer = SymbolSerializer(symbols, many=True)
+        # Return response
+        return Response(serializer.data)
 
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse([symbol.serialize() for symbol in symbols], safe=False)
 
-
-def symbol(request, stock_symbol):
-    # Get Symbols
-    symbol = Symbol.objects.get(symbol=stock_symbol.upper())
-
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse(symbol.serialize(), safe=False)
+class SymbolView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get URL parameter
+        stock_symbol = kwargs.get("stock_symbol")
+        # Get symbol
+        symbol = Symbol.objects.get(symbol=stock_symbol)
+        # Serialize symbol
+        serializer = SymbolSerializer(symbol)
+        # Return response
+        return Response(serializer.data)
 
 
 def update_symbols(request):
@@ -166,33 +184,34 @@ def update_symbols(request):
     util.update_symbols()
 
 
-def stocks(request):
-    # Get Stocks
-    stocks = Stock.objects.all()
+class StocksView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get Stocks
+        stocks = Stock.objects.all()
+        # Serialize stocks
+        serializer = StockSerializer(stocks, many=True)
+        # Return response
+        return Response(serializer.data)
 
-    # For a GET request
-    if request.method == "GET":
-        return JsonResponse([stock.serialize() for stock in stocks], safe=False)
 
-
-def stock(request, stock_symbol):
-
-    # For a GET request
-    if request.method == "GET":
+class StockView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Get URL parameter
+        stock_symbol = kwargs.get("stock_symbol")
 
         # Check if stock exists
         if Stock.objects.filter(symbol=stock_symbol.upper()).exists():
             # If the stock exists, update its data
             stock = Stock.objects.get(symbol=stock_symbol.upper())
 
-            # Return json
-            return JsonResponse(stock.serialize(), safe=False)
         else:
             # Create a new stock
-            new_stock = util.create_stock(stock_symbol)
+            stock = util.create_stock(stock_symbol)
 
-            # Return json
-            return JsonResponse(new_stock.serialize(), safe=False)
+        # Serialize stock
+        serializer = StockSerializer(stock)
+        # Return response
+        return Response(serializer.data)
 
 
 def update_stocks(request):
